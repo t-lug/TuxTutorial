9585906eb35433009e202900f212dfd159ba9b72
T-LUG (TKIET - LINUX USER GROUP) <tlug@tkietwarana.ac.in>
2025-02-06 00:43:05 +0530

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+def greet(name):
+    """Greet the user with their name."""
+    return f"Hello, {name}! Welcome to the Git and GitHub workshop."
+
+def add_numbers(a, b):
+    """Add two numbers and return the result."""
+    return a + b
+
+if __name__ == "__main__":
+    print(greet("Participant"))
+    print("Adding 5 and 3:", add_numbers(5, 3))
